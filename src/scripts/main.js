a0e812882793843562fc7cc07c9d546657805069
Grigoriy Nikitenko <grigonik22@gmail.com>
2025-11-14 12:50:14 +0100

    The header is finished

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
-'use strict';
+import Swiper from 'swiper';
+import 'swiper/swiper-bundle.min.css';
+import { Navigation } from 'swiper/modules';
+
+
+const swiper = new
+Swiper('.header-bottom__slider', {
+  modules: [Navigation],
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.header-bottom__btn--next',
+    prevEl: '.header-bottom__btn--prev',
+  },
+  autoplay: {
+    delay: 4000,
+  },
+  speed: 800,
+});
